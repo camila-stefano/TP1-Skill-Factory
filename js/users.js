fd688fbce822d53177488683be2ee7b862a88306
Alan Vilalte <alan9106@gmail.com>
2022-08-09 22:50:03 -0300

    :sparkles: Add users table

---
+++ js/users.js
@@ -0,0 +1,35 @@
+const table = document.querySelector("table");
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+const getUsers = async () => {
+  try {
+    const users = await axios.get(URL);
+    users.data.forEach((user) => {
+      const { name, id, email, phone } = user;
+      const { city } = user.address;
+      const { name: company } = user.company;
+
+      const newRow = document.createElement("tr");
+      const tdName = document.createElement("td");
+      const tdId = document.createElement("td");
+      const tdEmail = document.createElement("td");
+      const tdPhone = document.createElement("td");
+      const tdCity = document.createElement("td");
+      const tdCompany = document.createElement("td");
+
+      tdName.innerHTML = name;
+      tdId.innerHTML = id;
+      tdEmail.innerHTML = email;
+      tdPhone.innerHTML = phone;
+      tdCity.innerHTML = city;
+      tdCompany.innerHTML = company;
+
+      newRow.append(tdName, tdId, tdEmail, tdPhone, tdCity, tdCompany);
+      table.append(newRow);
+    });
+  } catch (err) {
+    console.log(`%c Error.${err}`, "background-color: red; color: white");
+  }
+};
+
+getUsers();
