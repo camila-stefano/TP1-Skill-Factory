71423b60e2679d64313265e9d182424836ba1cc4
Alan Vilalte <alan9106@gmail.com>
2022-08-12 05:08:56 -0300

    :art: Form styles

--- js/form.js
+++ js/form.js
@@ -3,14 +3,26 @@ const previewCard = document.querySelector(".preview");
 const previewBtn = document.querySelector(".preview-btn");
 const userNameInput = document.querySelector("#uname");
 const emailInput = document.querySelector("#email");
+const msgInput = document.querySelector("#msg");
+const messages = [];
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  preview.classList.add("preview-show");
 });
 
 previewBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  const msgPreview = {};
+  msgPreview.name = userNameInput.value;
+  msgPreview.email = emailInput.value;
+  msgPreview.msg = msgInput.value;
+  messages.push(msgPreview);
+  previewCard.innerHTML = `<p>Thank you! This is your message: </p>
+  <div><p><b>Username:</b> ${msgPreview.name}</p>
+    <p><b>E-mail:</b> ${msgPreview.email}</p>
+    <p><b>Message:</b> ${msgPreview.msg}</p></div>
+    <p><b>Click Submit to finish</p>`;
+
   previewCard.classList.add("preview-show");
 });
 
